2708e0c8c588041f55e51d093cefc207b42d24f4
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-21 21:57:26 -0400

    Add mb after about section

--- components/section/aboutSection/AboutSection.tsx
+++ components/section/aboutSection/AboutSection.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { FunctionComponent } from 'react';
 
 const AboutSection: FunctionComponent = () => (
-  <div className='flex flex-col w-1/2 mx-auto mt-20 justify-center items-center text-center'>
+  <div className='flex flex-col w-1/2 mx-auto mt-20 mb-12 justify-center items-center text-center'>
     <h1 className='mb-4 text-4xl text-center'>About Bitcoin</h1>
     <span>The world’s first cryptocurrency, Bitcoin is stored and exchanged securely on the internet through a digital ledger known as a blockchain. Bitcoins are divisible into smaller units known as satoshis — each satoshi is worth 0.00000001 bitcoin.</span>
     <h2 className='text-2xl mt-8 mb-2'>Resources</h2>
