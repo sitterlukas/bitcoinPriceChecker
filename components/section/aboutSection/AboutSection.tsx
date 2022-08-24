bc5d9f96b0a5138e734d81fab9caf3b4c1a26cda
Lukáš Sitter <lukassitter@Lukass-MacBook-Pro.local>
2022-08-23 21:36:35 -0400

    Add underline on hover to links

--- components/section/aboutSection/AboutSection.tsx
+++ components/section/aboutSection/AboutSection.tsx
@@ -7,8 +7,12 @@ const AboutSection: FunctionComponent = () => (
     <span>The world’s first cryptocurrency, Bitcoin is stored and exchanged securely on the internet through a digital ledger known as a blockchain. Bitcoins are divisible into smaller units known as satoshis — each satoshi is worth 0.00000001 bitcoin.</span>
     <h2 className='text-2xl mt-8 mb-2'>Resources</h2>
     <div className='flex flex-col gap-1'>
-      <Link href='https://bitcoin.org/bitcoin.pdf'>White paper</Link>
-      <Link href='https://bitcoin.org/'>Official website</Link>
+      <Link href='https://bitcoin.org/bitcoin.pdf'>
+        <a className='hover:underline hover:underline-offset-2' href='https://bitcoin.org/bitcoin.pdf'>White paper</a>
+      </Link>
+      <Link href='https://bitcoin.org/'>
+        <a className='hover:underline hover:underline-offset-2' href='https://bitcoin.org/'>Official website</a>
+      </Link>
     </div>
 
   </div>
